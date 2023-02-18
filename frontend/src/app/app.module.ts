d60e906c24f0184f0a5ad00ec68a39fbd380ebfe
Felix Melzer <felix.melzer@schmiedefeld.eu>
2023-02-18 21:43:22 +0100

    home bot nav build

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'
+import {MatListModule} from '@angular/material/list';
+
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeHeadComponent } from './components/home-head/home-head.component';
@@ -28,6 +30,7 @@ const matModules = [
   MatFormFieldModule,
   MatInputModule,
   FormsModule,
+  MatListModule
 ]
 
 @NgModule({
