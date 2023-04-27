f052d9dc38b79a594026148d8d37c48b65e64268
Felix Melzer <felix.melzer@schmiedefeld.eu>
2023-04-27 19:33:46 +0200

    rating question build

--- frontend/src/app/components/survey/rating-question/rating-question.component.ts
+++ frontend/src/app/components/survey/rating-question/rating-question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-rating-question',
@@ -7,9 +8,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingQuestionComponent implements OnInit {
 
+  current_question;
+
+  state;
+  msg;
+  action_text;
+
+  currOptText;
+
+  currOptions = [];
+
+  isRequired;
+  styleType;
+
+  minRateText;
+  maxRateText;
+
+  surveyJson;
+
+  @Output() getValueEvent = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addOption(value: string){
+    this.currOptions.push(value);    
+    this.currOptText = '';
+  }
+
+  clearOption(){
+    this.currOptions = [];
+  }
+
+  setQuestion(form: NgForm){  
+
+     //hacky lol
+     if(!(this.checkInput(form.value.textValue))) {
+
+      this.state = "err";
+      this.msg = "err set: txt null";
+      return;
+    }
+
+    this.current_question = {
+      "name":"test",
+      "type":"rating",
+      "title":form.value.textValue,
+      
+    };    
+
+    this.state = 'set';
+  }
+
+  preview() {
+    this.surveyJson = {      
+      elements: [this.current_question]
+    }
+  }
+
+  sendValue(form: NgForm){   
+
+    this.current_question.isRequired = this.isRequired;
+
+    if(this.styleType != "default") {
+      this.current_question.displayMode = this.styleType;
+    }
+
+    if(this.minRateText != null) {
+      this.current_question.minRateDescription = this.minRateText;
+    }
+
+    if(this.maxRateText != null){
+      this.current_question.maxRateDescription = this.maxRateText;
+    }
+
+    this.getValueEvent.emit(this.current_question)
+    this.clearValues(form);
+
+    this.msg = "success set: " + this.current_question.name + ' ' + this.current_question.state;
+    this.action_text = "add another question"
+  }
+
+  checkInput(value: string) {
+    if(value == null || value == '') { return false}
+    return true
+
+  }
+
+  clearValues(form: NgForm){
+    this.current_question = null;    
+    this.state = null;
+    this.msg = null;
+    this.surveyJson = null;
+    form.resetForm();
+  }
 }
