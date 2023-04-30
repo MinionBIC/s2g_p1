a8b1953e9f1a833bc3e5949269a4622c01f288c2
Felix Melzer <felix.melzer@schmiedefeld.eu>
2023-04-30 22:19:45 +0200

    fix leak mongoose

--- db/online_connect.js
+++ db/online_connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://dummy123:dummy123!@meanauthapp.wuvnd.mongodb.net/s2g_proto', { useNewUrlParser: true}).then(() => {
+mongoose.connect('', { useNewUrlParser: true}).then(() => {
     console.log('Connected to MongoDB Localhost');
     mongoose.set('strictQuery', true);
 }).catch((e) => {
